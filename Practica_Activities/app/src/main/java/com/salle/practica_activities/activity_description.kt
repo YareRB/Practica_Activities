2ac823736cd855857860ae1ef6b529681978fdde
Yare <yareram78@gmail.com>
2021-09-07 15:28:15 -0500

    Añadir imagen Favorita
    Permite usar shared preferences para guardar una imagen a favorito

--- Practica_Activities/app/src/main/java/com/salle/practica_activities/activity_description.kt
+++ Practica_Activities/app/src/main/java/com/salle/practica_activities/activity_description.kt
@@ -1,18 +1,29 @@
 package com.salle.practica_activities
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.moshi.Moshi
 
 class activity_description : AppCompatActivity() {
 
     private var image: Image = Image()
 
+    private val PREFS = "MY_PREFERENCES"
+    private val IMAGE_PREFS = "IMAGE_PREFS"
+    private lateinit var preferences: SharedPreferences //variable global
+
+    private val moshi = Moshi.Builder().build() //JSON
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_description)
+
+        preferences = getSharedPreferences(PREFS, Context.MODE_PRIVATE) //cuales son preferencias
 
         InitView()
     }
@@ -36,6 +47,13 @@ class activity_description : AppCompatActivity() {
     private fun showInformation() {
         imgObjectD.setImageResource(image.info!!.resource)
         txvDescription.setText(image.info!!.description)
+
+        var imageFavorite : Int = getPreferences()
+        if(imageFavorite == image.id){
+            imgFavorite.setImageResource(R.drawable.ic_favorite)
+        }else{
+            imgFavorite.setImageResource(R.drawable.ic_no_favorite)
+        }
     }
 
 
@@ -45,7 +63,32 @@ class activity_description : AppCompatActivity() {
                 putExtra("Image", image)
             })
         }
+
+        imgFavorite.setOnClickListener(){
+            var idFavorite : Int = getPreferences()
+            if(idFavorite == image.id){
+                //quita el fav
+                savePreference(-1)}
+            else{
+                savePreference(image.id) //agrega el fav
+            }
+        }
     }
 
+    private fun getPreferences() =
+        preferences.getString(IMAGE_PREFS, null)?.let {
+            return@let try {
+                moshi.adapter(Int::class.java).fromJson(it) // guardar en json, y lo regresamos
+            } catch (e: Exception) {
+                -1
+            }
+        } ?: -1
 
+    private fun savePreference( value: Int? = -1) {
+
+        preferences.edit().putString(IMAGE_PREFS, moshi.adapter(Int::class.java).toJson(value))
+            .apply()//se apliquen los cambios
+
+        showInformation()
+    }
 }
