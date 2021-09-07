18249a9beb7804891f72a8bdf43ec50f22916bd7
Yare <yareram78@gmail.com>
2021-09-07 14:13:57 -0500

    Imágenes, string y arreglo
    Se agrego las imágenes en sus tamaños, creación de strings y termino del arreglo con las imágenes

--- Practica_Activities/app/src/main/java/com/salle/practica_activities/Description.kt
+++ Practica_Activities/app/src/main/java/com/salle/practica_activities/Description.kt
@@ -2,6 +2,14 @@ package com.salle.practica_activities
 
 enum class Description(val resource: Int, val description: Int) {
     Cerdo(R.drawable.cerdo, R.string.text_cerdo),
+    Oso(R.drawable.oso, R.string.text_oso),
+    Pez(R.drawable.pez, R.string.text_pez),
+    Girafa(R.drawable.girafa, R.string.tetx_girafa),
+    Perro(R.drawable.perro, R.string.text_perro),
     Android(R.drawable.ic_android, R.string.text_android),
-    Facebook(R.drawable.ic_facebook, R.string.text_facebook)
+    Facebook(R.drawable.ic_facebook, R.string.text_facebook),
+    Linux(R.drawable.ic_linux, R.string.text_linux),
+    Netflix(R.drawable.ic_netflix, R.string.text_netflix),
+    Windows(R.drawable.ic_windows, R.string.text_windows)
+
 }
--- Practica_Activities/app/src/main/java/com/salle/practica_activities/Image.kt
+++ Practica_Activities/app/src/main/java/com/salle/practica_activities/Image.kt
@@ -13,7 +13,14 @@ class Image (
         val images = arrayOf(
             Image(0, "Cerdo", info = Description.Cerdo),
             Image(1, "Facebook", info = Description.Facebook),
-            Image(2, "Android", info = Description.Android)
+            Image(2, "Perro", info = Description.Perro),
+            Image(3, "Android", info = Description.Android),
+            Image(4, "Girafa", info = Description.Girafa),
+            Image(5, "Linux", info = Description.Linux),
+            Image(6, "Pez", info = Description.Pez),
+            Image(7, "Windows", info = Description.Windows),
+            Image(8, "Oso", info = Description.Oso),
+            Image(9, "Netflix", info = Description.Netflix),
         )
     }
         fun setImage(id: Int): Image = images[id]
